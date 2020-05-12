ef1e8332277df5bd1c91e2c1a547e9d72781c7db
Irfan Khan <subhashnic>
2020-05-12 11:29:04 +0530

    Commit by Irfan

---
+++ PGMSFront/Controllers/FrontController.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PGMSFront.Controllers
+{
+    public class FrontController : Controller
+    {
+        // GET: Front
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BrakingTrack()
+        {
+            return View();
+        }
+
+        public ActionResult TestHill()
+        {
+            return View();
+        }
+
+        public ActionResult FatigueSurfaceHighSeverity()
+        {
+            return View();
+        }
+
+        public ActionResult FatigueStructure()
+        {
+            return View();
+        }
+
+        public ActionResult GravelandOffRoad()
+        {
+            return View();
+        }
+
+        public ActionResult HandlingTrackfor4W()
+        {
+            return View();
+        }
+
+        public ActionResult HandlingTrackfor2W()
+        {
+            return View();
+        }
+
+        public ActionResult ComfortTrack()
+        {
+            return View();
+        }
+
+        public ActionResult SustainabilityTrack()
+        {
+            return View();
+        }
+
+        public ActionResult WetSkidPad()
+        {
+            return View();
+        }
+
+        public ActionResult ExternalNoiseTrack()
+        {
+            return View();
+        }
+
+        public ActionResult VillageRoadTrack()
+        {
+            return View();
+        }
+
+        public ActionResult GeneralRoad()
+        {
+            return View();
+        }
+
+        public ActionResult Track()
+        {
+            return View();
+        }
+
+        public ActionResult Lab()
+        {
+            return View();
+        }
+
+        public ActionResult Workshop()
+        {
+            return View();
+        }
+
+        public ActionResult Storage()
+        {
+            return View();
+        }
+
+        public ActionResult AddOnServices()
+        {
+            return View();
+        }
+
+        public ActionResult TrackDetail()
+        {
+            return View();
+        }
+
+        public ActionResult BookWorkshop()
+        {
+            return View();
+        }
+
+        public ActionResult BookStorage()
+        {
+            return View();
+        }
+
+        public ActionResult CompanyRegistration()
+        {
+            return View();
+        }
+
+
+        public ActionResult HighSpeedTrackDetail()
+        {
+            return View();
+        }
+
+        public ActionResult DynamicPlatformDetail()
+        {
+            return View();
+        }
+
+        public ActionResult BrakingTrackDetail()
+        {
+            return View();
+        }
+
+        public ActionResult TestHillDetail()
+        {
+            return View();
+        }
+
+        public ActionResult FatigueTrackDetail()
+        {
+            return View();
+        }
+
+        public ActionResult GravelDetail()
+        {
+            return View();
+        }
+
+        public ActionResult HandlingTrack4WDetail()
+        {
+            return View();
+        }
+
+        public ActionResult ComfortTrackDetail()
+        {
+            return View();
+        }
+
+        public ActionResult HandlingTrack2WDetail()
+        {
+            return View();
+        }
+
+        public ActionResult SustainabilityTrackDetail()
+        {
+            return View();
+        }
+
+        public ActionResult WetSkidPadDetail()
+        {
+            return View();
+        }
+
+        public ActionResult VillageRoadDetail()
+        {
+            return View();
+        }
+
+        public ActionResult ExternalNoiseTrackDetail()
+        {
+            return View();
+        }
+
+        public ActionResult GeneralRoadDetail()
+        {
+            return View();
+        }
+
+        public ActionResult InstrumentationLabDetail()
+        {
+            return View();
+        }
+
+        public ActionResult VehicleDyanmicsLabDetail()
+        {
+            return View();
+        }
+
+        public ActionResult PowerTrainLabDetail()
+        {
+            return View();
+        }
+
+        public ActionResult CADCAELabDetail()
+        {
+            return View();
+        }
+
+        public ActionResult ClientWorkshopDetail()
+        {
+            return View();
+        }
+
+        public ActionResult GeneralStorageDetail()
+        {
+            return View();
+        }
+
+        public ActionResult AddOnServicesDetail()
+        {
+            return View();
+        }
+
+        public ActionResult GeneralStorage()
+        {
+            return View();
+        }
+
+        public ActionResult BookPowerTrainLab()
+        {
+            return View();
+        }
+
+        public ActionResult VehicleDynamicsLabVehiclewise()
+        {
+            return View();
+        }
+
+        public ActionResult VehicleDynamicsLabComponentwise()
+        {
+            return View();
+        }
+
+        public ActionResult InstrumentationLabVehiclewise()
+        {
+            return View();
+        }
+
+        public ActionResult InstrumentationLabComponentwise()
+        {
+            return View();
+        }
+
+        public ActionResult InstrumentationLab()
+        {
+            return View();
+        }
+
+        public ActionResult VehicleDynamicsLab()
+        {
+            return View();
+        }
+
+        public ActionResult PowerTrainLab()
+        {
+            return View();
+        }
+
+        public ActionResult CADCAELab()
+        {
+            return View();
+        }
+
+        public ActionResult ElectricalBatteryTestingFacilities()
+        {
+            return View();
+        }
+
+        public ActionResult ElectricalVehicleTestingLab()
+        {
+            return View();
+        }
+
+        public ActionResult KinamaticsComplianceMachineDetail()
+        {
+            return View();
+        }
+
+        public ActionResult SteeringTestRigDetail()
+        {
+            return View();
+        }
+
+        public ActionResult ElastomerTestRigDetail()
+        {
+            return View();
+        }
+
+        public ActionResult DamperTestRigDetail()
+        {
+            return View();
+        }
+
+        public ActionResult ChassisDynoDetail()
+        {
+            return View();
+        }
+
+        public ActionResult EmissionAnalyzersDetail()
+        {
+            return View();
+        }
+
+        public ActionResult WorkstationsDetails()
+        {
+            return View();
+        }
+
+        public ActionResult UnderConstruction()
+        {
+            return View();
+        }
+    }
+}
